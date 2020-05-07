0537f53c521359b7fa95b07ba529e0ed5d21968b
Joseph Murphy <jrmurphy@wpi.edu>
2020-05-07 17:18:48 -0400

    add main loop functionality

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,16 @@
 
 void setup()
 {
-  
+    Serial.begin(115200);       // Start the Serial communication buad rate 115200
+    delay(10);
+    Serial.println('\n');
+
+    gpioInit();                 // initialize
+    wifiInit();
+    serverInit();
 }
 
 void loop()
 {
-  
+  server.handleClient();
 }
