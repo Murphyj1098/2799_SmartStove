ec62047191c7cbb0a256a20333bb9db02006f01f
Joseph Murphy <jrmurphy@wpi.edu>
2020-05-07 17:18:21 -0400

    Add server handler prototypes

--- include/serverHandlers.h
+++ include/serverHandlers.h
@@ -8,6 +8,9 @@
 
 
 /*function prototypes*/
-
+// reponds to GET request with state of stove (GPIO0)
+void handleState(void);
+// on POST turns off the stove with motor (GPIO2)
+void handleMotor(void);
 
 #endif
--- src/serverHandlers.cpp
+++ src/serverHandlers.cpp
@@ -1,4 +1,16 @@
 #include <Arduino.h>
 
+#include "initialization.h"
 #include "serverHandlers.h"
 
+// reponds to GET request with state of stove (GPIO0)
+void handleState(void)
+{
+
+}
+
+// on POST turns off the stove with motor (GPIO2)
+void handleMotor(void)
+{
+
+}
